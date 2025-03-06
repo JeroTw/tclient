72accefd952bf059fb34d96ecd2cf352f5356af6
Kirill <119045988+JeroTw@users.noreply.github.com>
2025-03-06 21:41:23 +0800

    Update version.h

--- src/game/version.h
+++ src/game/version.h
@@ -13,7 +13,7 @@
 #define GAME_NETVERSION7 "0.7 802f1be60a05665f"
 
 // ddnet
-#define DDNET_VERSION_NUMBER 19000
+#define DDNET_VERSION_NUMBER 19088
 extern const char *GIT_SHORTREV_HASH;
 #define GAME_NAME "DDNet"
 #define CLIENT_NAME "TClient"
